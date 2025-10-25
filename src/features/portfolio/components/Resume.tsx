30d0d9d428edb0b93f16a63614c9d26d44291d9e
Micah Stalberg <micahrain2000@gmail.com>
2025-10-25 12:53:45 -0700

    Add and configure tailwind

--- src/features/portfolio/components/Resume.tsx
+++ src/features/portfolio/components/Resume.tsx
@@ -1,6 +1,6 @@
 const Resume = () => {
     return (
-        <div>
+        <div className="bg-gray-300">
             <h1>My Resume</h1>
             <p>Here is a summary of my professional experience...</p>
         </div>
