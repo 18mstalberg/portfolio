d2bc43fdd63e5a40308030e5f607fc0361b5be18
Micah Stalberg <micahrain2000@gmail.com>
2025-10-26 12:42:00 -0700

    Update fixed screen height to be apply only to greater the md screens

--- src/app/routes/app/portfolio.tsx
+++ src/app/routes/app/portfolio.tsx
@@ -3,15 +3,10 @@ import Resume from "../../../features/portfolio/components/Resume";
 
 const PortfolioRoute = () => {
     return (
-        <main className="h-screen p-4 sm:p-8 lg:p-16">
-            <div className="h-full mx-auto flex w-full flex-col gap-12 lg:flex-row lg:items-start">
-                <div className="h-full lg:sticky lg:top-10 ">
-                    <ProfileCard />
-                </div>
-
-                <section className="flex-1 space-y-8">
-                    <Resume />
-                </section>
+        <main className="md:h-screen p-4 sm:p-8 lg:p-16">
+            <div className="h-full mx-auto flex w-full flex-col gap-12 md:flex-row md:items-start">
+                <ProfileCard />
+                <Resume />
             </div>
         </main>
     );
--- src/features/portfolio/components/Resume.tsx
+++ src/features/portfolio/components/Resume.tsx
@@ -1,8 +1,8 @@
 const Resume = () => {
     return (
-        <div className="">
+        <div className="h-full w-full md:overflow-y-auto rounded-3xl border border-(--border) bg-(--bg-elevated) sm:p-8 p-4">
             <h2 className="text-2xl">My Resume</h2>
-            <p>Here is a summary of my professional experience...</p>
+            <p>Here is a summary of my professional experience...</p>          
         </div>
     );
 }
